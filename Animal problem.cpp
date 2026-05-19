12904a53f260ee2b19d0fdec31a7ebfc9b4c27f5
Sheikh Mohammad Shakib <skmdshakib00@gmail.com>
2026-05-19 14:17:40 +0600

    Add files via upload

---
+++ Animal problem.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class animal{
+public:
+    void eat(){
+    cout<<"Eating"<<endl;
+    }
+};
+class bird:public animal{
+public:
+   void  fly(){
+    cout<<"Flying "<<endl;
+    }
+
+};
+class sparrow:public bird{
+public:
+    void chirp(){
+    cout<<"Chirping"<<endl;
+    }
+
+};
+int main(){
+
+ sparrow ob;
+
+ ob.chirp();
+
+}
